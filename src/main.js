4ac98a36bf2c8d7e4b4154301a501f00c7502764
kingsley Diala <donbuski2@gmail.com>
2023-12-10 16:44:06 +0100

    Remove redundant code

--- src/main.js
+++ src/main.js
@@ -2,12 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { iconsSet as icons } from "@coreui/icons";
-import { CIcon } from "@coreui/icons-vue";
 
 createApp(App)
   .use(store)
   .use(router)
-  .component("c-icon", CIcon)
-  .use(icons)
   .mount("#app")
